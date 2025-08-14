cb367ca148036baf3f4c023e132c7c39336e7bce
AryanRogye <161079183+AryanRogye@users.noreply.github.com>
2025-08-14 00:15:37 -0500

    just making the example better

--- Examples/ComfyMarkdownExample/ComfyMarkdownExample/ContentView.swift
+++ Examples/ComfyMarkdownExample/ComfyMarkdownExample/ContentView.swift
@@ -47,10 +47,10 @@ struct ContentView: View {
             
             ---
             
-            - Item 1
+            - # Item 1
               - Item 2
                 - item 3
-              - item 2
+              - ## item 2
             - item 3
             
             """, maxFontSize: $fontsize)
